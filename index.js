b89c14ec22d88452a1bff3f9502ef01572aba82b
manuelprg <manolo.contreras@idbgroup.net>
2016-03-10 12:48:11 +0100

    añadimos sql

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ var http = require('http');
 
 var mysql = require('mysql');
 
-var server = http.createServer(function(req, res) {
-  res.end('text');
+var db = mysql.createPool({
+  host:'localhost',
+  user: 'root',
+  password: '',
+  database:'idb_esme_new',
+  //connectionLimit
 });
+
+
+
+
+
+
+var server = http.createServer(function(req, res) {
+  db.query('select * from country', function(err, res) {
+
+  });
+});
+/*
+connection.connect();
+
+connection.query('SELECT * from country', function(err, rows, fields) {
+ if (err) throw err;
+
+ console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();*/
 
 server.listen(1234);
